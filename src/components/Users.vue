99341b7c864def49706593f4fdaef502e8ece58e
MeXos5 <mexanikus5@gmail.com>
2017-07-01 16:41:40 +0300

    Добавил прототип таблицы (крайне черновой)

---
+++ src/components/Users.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="Users">
+    <div class="container">
+        <table class="table">
+            <tr>
+                <th>Name</th>
+                <th>Stat</th>
+            </tr>
+            <tbody>
+                <tr v-for="users in users_list">
+                    <td>{{users.name}}</td>
+                    <td>{{users.status}}</td>
+                </tr>
+            </tbody>
+        </table>
+        {{users_list}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      users_list: [
+        {
+          'name': 'Ainz',
+          'status': 'Overlord'
+        },
+        {
+          'name': 'Albedo',
+          'status': 'General'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>

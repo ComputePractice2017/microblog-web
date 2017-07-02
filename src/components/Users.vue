7394ab054986b4aa1596aa43b8216e6785ca24a4
MeXos5 <mexanikus5@gmail.com>
2017-07-02 13:09:43 +0300

    Повторный коммит с лентой

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,19 +1,40 @@
+import '../CSS/Styles.css'
 <template>
   <div class="Users">
-    <div class="container">
-        <table class="table">
-            <tr>
-                <th>Name</th>
-                <th>My thoughts</th>
-            </tr>
-            <tbody>
-                <tr v-for="users in users_list">
-                    <td>{{users.name}}</td>
-                    <td>{{users.message}}</td>
-                </tr>
-            </tbody>
-        </table>
-        {{users_list}}
+    <div class="container-fluid" style="background-color:black;">
+      <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#">Микро-блог</a>
+          </div>
+          <ul class="nav navbar-nav">
+            <li class="active">
+              <a href="#">Мой профиль</a>
+            </li>
+          </ul>
+          <form class="navbar-form navbar-left">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Что Вам угодно?">
+            </div>
+            <button type="submit" class="btn btn-default">Сейчас найдём!</button>
+          </form>
+        </div>
+      </nav>
+      <div class="col-sm-12 col-md-12" style="background-color:lavender;">
+        Лента
+        <div class="row">
+          <div class="col-sm-6 col-md-3 supp" style="background-color:grey;">Предложка</div>
+          <div class="col-sm-6 col-md-6 feed" style="background-color:pink;">
+            <div class="row">
+              <div class="col-sm-3 col-md-3 col-lg-12">Твит 1</div>
+              <div class="col-sm-3 col-md-3 col-lg-12">Твит 2</div>
+              <div class="col-sm-3 col-md-3 col-lg-12">Твит 3</div>
+              <div class="col-sm-3 col-md-3 col-lg-12">Твит 4</div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-3 filter" style="background-color:orange;">Фильтр</div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -39,8 +60,5 @@ export default {
 </script>
 
 <style>
-.container {
-  margin-top: 30px;
-  margin-bottom: 30px;
-}
+
 </style>
